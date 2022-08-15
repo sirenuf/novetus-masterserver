d7fd8fd1d32138719ce9beca5cd8345ec0615f74
enuf96 <master@viw.se>
2022-08-15 20:12:51 +0200

    fix proxy

--- masterserver/server.py
+++ masterserver/server.py
@@ -2,12 +2,14 @@ from threading import Thread
 from time import sleep, monotonic
 from routes import endpoint
 from routes.endpoint import serverList as sL
+from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import (
     Flask
 )
 
 app = Flask(__name__)
 app.register_blueprint(endpoint.bp)
+app = ProxyFix(app, x_for=1, x_proto=1, x_host=1)
 
 # TODO: Make more readable.
 def routine():
@@ -17,8 +19,8 @@ def routine():
             if monotonic() - value["keepAlive"] >= 15:
                 sL.pop(key, None)
 
-if __name__ == "__main__":
-    task = Thread(target=routine, daemon=True)
-    task.start()
+task = Thread(target=routine, daemon=True)
+task.start()
 
-    app.run() # debug=true makes the thread run twice... not issue for production
+if __name__ == "__main__":
+    app.run(port=8000) # debug=true makes the thread run twice... not issue for production
